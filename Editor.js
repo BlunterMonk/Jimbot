747ab9b1b95e4522dc4ac79aedaf1e4eabf7c7a2
SIREN\Dahmitri <Dahmitri.S@Gmail.com>
2019-04-26 01:50:10 -0400

    refactor WIP

--- Editor.js
+++ Editor.js
@@ -3,9 +3,10 @@ const config = require("./config/config.ts");
 const String = require("./string/string-extension.ts");
 
 class Editor {
-    constructor(userId) {
+    constructor(guildId, userId) {
         this.userId = userId;
-        
+        this.guildId = guildId;
+        this.settings;
     }
 
     
