046522ea45ecd3d95ee0dbb25d9363eb74fd2c84
SIREN\Dahmitri <Dahmitri.S@Gmail.com>
2019-04-26 00:24:47 -0400

    WIP

---
+++ Editor.js
@@ -0,0 +1,14 @@
+
+const config = require("./config/config.ts");
+const String = require("./string/string-extension.ts");
+
+class Editor {
+    constructor(userId) {
+        this.userId = userId;
+        
+    }
+
+    
+}
+
+module.exports = Editor;
