ab5615d550110843e2d174045b373dfc2d882c47
SIREN\Dahmitri <Dahmitri.S@Gmail.com>
2019-04-29 14:08:10 -0400

    quotes fix

--- src/jimbot.ts
+++ src/jimbot.ts
@@ -1420,7 +1420,7 @@ function getCommandString(msg, prefix) {
 function getParameters(msg) {
 
     var parameters = [];
-    var params = msg.match(/"[^"]+"/g);
+    var params = msg.match(/"[^"]+"|‘[^‘]+‘|‘[^’]+’|“[^“]+“|”[^”]+”|“[^“^”]+”/g);
     if (params) {
         parameters = params;
 
