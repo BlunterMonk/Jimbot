3d7ff4698b23876031636ed18a720a6e7a77f153
SIREN\Dahmitri <Dahmitri.S@Gmail.com>
2019-05-12 12:02:38 -0400

    wip

---
+++ src/ffbe/unit.ts
@@ -0,0 +1,18 @@
+//////////////////////////////////////////
+// Author: Dahmitri Stephenson
+// Discord: Jimoori#2006
+// Jimbot: Discord Bot
+//////////////////////////////////////////
+
+import { log, logData,
+    checkString, compareStrings,
+    escapeString } from "../global.js";
+import "../string/string-extension.js";
+import * as fs from "fs";
+import * as https from "https";
+import * as http from "http";
+
+
+export function unitSearch(name: string, parameters): any {
+
+}
